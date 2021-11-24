8baeb3643a1d11c38087dc2d1ef03cf3e649a75f
Alvaro Merino <alvaro.merino.fernandez@gmail.com>
2021-11-24 12:23:54 +0100

    Practica terminada

--- SGClubRaquetaAlvaroMerino/frmAdminSocios.Designer.cs
+++ SGClubRaquetaAlvaroMerino/frmAdminSocios.Designer.cs
@@ -31,7 +31,6 @@ namespace SGClubRaquetaAlvaroMerino
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdminSocios));
             this.label1 = new System.Windows.Forms.Label();
-            this.txtDni = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtApellidos = new System.Windows.Forms.TextBox();
@@ -44,6 +43,13 @@ namespace SGClubRaquetaAlvaroMerino
             this.label7 = new System.Windows.Forms.Label();
             this.mtxtTelefono = new System.Windows.Forms.MaskedTextBox();
             this.mtxtCCC = new System.Windows.Forms.MaskedTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.mktxtDni = new System.Windows.Forms.MaskedTextBox();
+            this.btnMostrarTodos = new System.Windows.Forms.Button();
+            this.dgvSocios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSocios)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -55,24 +61,17 @@ namespace SGClubRaquetaAlvaroMerino
             this.label1.TabIndex = 0;
             this.label1.Text = "DNI";
             // 
-            // txtDni
-            // 
-            this.txtDni.Location = new System.Drawing.Point(130, 27);
-            this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(93, 20);
-            this.txtDni.TabIndex = 1;
-            // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(130, 72);
+            this.txtNombre.Location = new System.Drawing.Point(406, 26);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(93, 20);
+            this.txtNombre.Size = new System.Drawing.Size(167, 20);
             this.txtNombre.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 75);
+            this.label2.Location = new System.Drawing.Point(341, 30);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 2;
@@ -80,15 +79,15 @@ namespace SGClubRaquetaAlvaroMerino
             // 
             // txtApellidos
             // 
-            this.txtApellidos.Location = new System.Drawing.Point(130, 121);
+            this.txtApellidos.Location = new System.Drawing.Point(651, 30);
             this.txtApellidos.Name = "txtApellidos";
-            this.txtApellidos.Size = new System.Drawing.Size(93, 20);
+            this.txtApellidos.Size = new System.Drawing.Size(137, 20);
             this.txtApellidos.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(58, 124);
+            this.label3.Location = new System.Drawing.Point(579, 33);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 4;
@@ -97,7 +96,7 @@ namespace SGClubRaquetaAlvaroMerino
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(58, 235);
+            this.label4.Location = new System.Drawing.Point(336, 79);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(64, 13);
             this.label4.TabIndex = 6;
@@ -105,15 +104,15 @@ namespace SGClubRaquetaAlvaroMerino
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(130, 282);
+            this.txtEmail.Location = new System.Drawing.Point(651, 76);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(93, 20);
+            this.txtEmail.Size = new System.Drawing.Size(137, 20);
             this.txtEmail.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(58, 285);
+            this.label5.Location = new System.Drawing.Point(579, 79);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(39, 13);
             this.label5.TabIndex = 8;
@@ -122,7 +121,7 @@ namespace SGClubRaquetaAlvaroMerino
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(58, 326);
+            this.label6.Location = new System.Drawing.Point(58, 123);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(28, 13);
             this.label6.TabIndex = 10;
@@ -130,15 +129,15 @@ namespace SGClubRaquetaAlvaroMerino
             // 
             // txtDomicilio
             // 
-            this.txtDomicilio.Location = new System.Drawing.Point(130, 175);
+            this.txtDomicilio.Location = new System.Drawing.Point(130, 76);
             this.txtDomicilio.Name = "txtDomicilio";
-            this.txtDomicilio.Size = new System.Drawing.Size(93, 20);
+            this.txtDomicilio.Size = new System.Drawing.Size(151, 20);
             this.txtDomicilio.TabIndex = 15;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(62, 175);
+            this.label7.Location = new System.Drawing.Point(62, 76);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(62, 13);
             this.label7.TabIndex = 14;
@@ -146,23 +145,89 @@ namespace SGClubRaquetaAlvaroMerino
             // 
             // mtxtTelefono
             // 
-            this.mtxtTelefono.Location = new System.Drawing.Point(128, 232);
+            this.mtxtTelefono.Location = new System.Drawing.Point(406, 76);
+            this.mtxtTelefono.Mask = "000-000-000";
             this.mtxtTelefono.Name = "mtxtTelefono";
-            this.mtxtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.mtxtTelefono.Size = new System.Drawing.Size(167, 20);
             this.mtxtTelefono.TabIndex = 16;
             // 
             // mtxtCCC
             // 
-            this.mtxtCCC.Location = new System.Drawing.Point(130, 323);
+            this.mtxtCCC.Location = new System.Drawing.Point(130, 120);
+            this.mtxtCCC.Mask = "ES00-0000-0000-0000-0000-0000";
             this.mtxtCCC.Name = "mtxtCCC";
-            this.mtxtCCC.Size = new System.Drawing.Size(100, 20);
+            this.mtxtCCC.Size = new System.Drawing.Size(177, 20);
             this.mtxtCCC.TabIndex = 17;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(353, 118);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 18;
+            this.btnAdd.Text = "Añadir";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(457, 118);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 19;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(582, 118);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // mktxtDni
+            // 
+            this.mktxtDni.Location = new System.Drawing.Point(130, 26);
+            this.mktxtDni.Mask = "00000000-L";
+            this.mktxtDni.Name = "mktxtDni";
+            this.mktxtDni.Size = new System.Drawing.Size(177, 20);
+            this.mktxtDni.TabIndex = 21;
+            // 
+            // btnMostrarTodos
+            // 
+            this.btnMostrarTodos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarTodos.Location = new System.Drawing.Point(61, 168);
+            this.btnMostrarTodos.Name = "btnMostrarTodos";
+            this.btnMostrarTodos.Size = new System.Drawing.Size(707, 62);
+            this.btnMostrarTodos.TabIndex = 22;
+            this.btnMostrarTodos.Text = "Mostrar Todos";
+            this.btnMostrarTodos.UseVisualStyleBackColor = true;
+            this.btnMostrarTodos.Click += new System.EventHandler(this.btnMostrarTodos_Click);
+            // 
+            // dgvSocios
+            // 
+            this.dgvSocios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvSocios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSocios.Location = new System.Drawing.Point(65, 256);
+            this.dgvSocios.Name = "dgvSocios";
+            this.dgvSocios.Size = new System.Drawing.Size(703, 182);
+            this.dgvSocios.TabIndex = 23;
             // 
             // frmAdminSocios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvSocios);
+            this.Controls.Add(this.btnMostrarTodos);
+            this.Controls.Add(this.mktxtDni);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.mtxtCCC);
             this.Controls.Add(this.mtxtTelefono);
             this.Controls.Add(this.txtDomicilio);
@@ -175,11 +240,11 @@ namespace SGClubRaquetaAlvaroMerino
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtDni);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmAdminSocios";
             this.Text = "Administrador de Socios";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSocios)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,7 +253,6 @@ namespace SGClubRaquetaAlvaroMerino
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtDni;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtApellidos;
@@ -201,5 +265,11 @@ namespace SGClubRaquetaAlvaroMerino
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.MaskedTextBox mtxtTelefono;
         private System.Windows.Forms.MaskedTextBox mtxtCCC;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.MaskedTextBox mktxtDni;
+        private System.Windows.Forms.Button btnMostrarTodos;
+        private System.Windows.Forms.DataGridView dgvSocios;
     }
 }
--- SGClubRaquetaAlvaroMerino/frmAdminSocios.cs
+++ SGClubRaquetaAlvaroMerino/frmAdminSocios.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SGClubRaquetaAlvaroMerino
@@ -15,6 +10,195 @@ namespace SGClubRaquetaAlvaroMerino
         public frmAdminSocios()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (comprobarCamposVacios())
+            {
+                string dni = mktxtDni.Text;
+                string nombre = txtNombre.Text;
+                string apellidos = txtApellidos.Text;
+                string domicilio = txtDomicilio.Text;
+                string telefono = mtxtTelefono.Text;
+                string email = txtEmail.Text;
+                string cuenta = mtxtCCC.Text;
+
+                if (comprobarSociosExistentes(dni))
+                {
+                    using (clubraquetaEntities objBd = new clubraquetaEntities())
+                    {
+                        socios objSocio = new socios();
+                        objSocio.DNI = dni;
+                        objSocio.nombre = nombre;
+                        objSocio.apellidos = apellidos;
+                        objSocio.domicilio = domicilio;
+                        objSocio.telefono = telefono;
+                        objSocio.email = email;
+                        objSocio.cuentaCorriente = cuenta;
+
+                        objBd.socios.Add(objSocio);
+                        MessageBox.Show("Socio añadido correctamente...");
+                        objBd.SaveChanges();
+                        LimpiarCampo();
+                    }
+                }
+            }
+        }
+
+        private void LimpiarCampo()
+        {
+            txtNombre.Text = "";
+            txtApellidos.Text = "";
+            txtDomicilio.Text = "";
+            txtEmail.Text = "";
+            mktxtDni.Text = "";
+            mtxtCCC.Text = "";
+            mtxtTelefono.Text = "";
+        }
+
+        /**
+         * Método que comprueba que no exista algún socio con el mismo DNI.
+         */
+        private bool comprobarSociosExistentes(string text)
+        {
+            using (clubraquetaEntities objBD = new clubraquetaEntities())
+            {
+                var consulta = from soc in objBD.socios
+                               where soc.DNI == text
+                               select soc;
+                if (consulta.Count() > 0)
+                {
+                    MessageBox.Show("Socio Existente");
+                    return false;
+                }
+                return true;
+
+            }
+        }
+
+        /**
+         * Método que comprueba que todas las máscaras están completas 
+         * y que todos los textBox tienen algún dato. 
+         */
+        private bool comprobarCamposVacios()
+        {
+            if (!mktxtDni.MaskCompleted || string.IsNullOrEmpty(mktxtDni.Text))
+            {
+                MessageBox.Show("El dni está incompleto");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtApellidos.Text) || string.IsNullOrEmpty(txtDomicilio.Text) || string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Rellene todos los campos antes de seguir");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(mtxtTelefono.Text) || !mtxtTelefono.MaskCompleted)
+            {
+                MessageBox.Show("Rellene el campo telefono");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(mtxtCCC.Text) || !mtxtCCC.MaskCompleted)
+            {
+                MessageBox.Show("Rellene el campo de la cuenta corriente");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnMostrarTodos_Click(object sender, EventArgs e)
+        {
+            using (clubraquetaEntities objBD = new clubraquetaEntities())
+            {
+                var consulta = from soc in objBD.socios
+                               select new { DNI = soc.DNI, NOMBRE = soc.nombre, 
+                                   APELLIDOS = soc.apellidos, DOMICILIO = soc.domicilio,
+                                   TELEFONO = soc.telefono, EMAIL = soc.email, CCC = soc.cuentaCorriente};
+
+                dgvSocios.DataSource = consulta.ToList();
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            string dni = mktxtDni.Text;
+            if (comprobarCamposVacios() && !comprobarSociosExistentes(dni))
+            {
+
+                string nombre = txtNombre.Text;
+                string apellidos = txtApellidos.Text;
+                string domicilio = txtDomicilio.Text;
+                string telefono = mtxtTelefono.Text;
+                string email = txtEmail.Text;
+                string cuenta = mtxtCCC.Text;
+
+
+                using (clubraquetaEntities objBd = new clubraquetaEntities())
+                {
+                    socios objSocio = objBd.socios.First(x => x.DNI.Equals(dni));
+                    objSocio.DNI = dni;
+                    objSocio.nombre = nombre;
+                    objSocio.apellidos = apellidos;
+                    objSocio.domicilio = domicilio;
+                    objSocio.telefono = telefono;
+                    objSocio.email = email;
+                    objSocio.cuentaCorriente = cuenta;
+                    objBd.SaveChanges();
+                    MessageBox.Show("Socio Actualizado correctamente");
+                }
+            }
+            else if(comprobarSociosExistentes(dni))
+            {
+                MessageBox.Show("El socio no existe.");
+            }else if (comprobarCamposVacios())
+            {
+                MessageBox.Show("Campos vacios");
+            }
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string dni = mktxtDni.Text;
+            if (!mktxtDni.MaskCompleted || string.IsNullOrEmpty(mktxtDni.Text))
+            {
+                MessageBox.Show("El dni está incompleto");
+
+            }
+            else
+            {
+                using (clubraquetaEntities objBD = new clubraquetaEntities())
+                {
+                    //Comprobamos si el socio existe
+                    bool existe = objBD.socios.Any(x => x.DNI.Equals(dni));
+                    if (existe)
+                    {
+                        //Ahora comprobamos si el socio tiene reservas de pista
+                        existe = objBD.reservas.Any(x => x.socio.Equals(dni));
+                        if (existe)
+                        {
+                            MessageBox.Show("El socio tiene alguna reserva, por tanto no se puede eliminar");
+                        }
+                        else
+                        {
+                            //Se elimina el objeto de la tabla, para quitarlo como registro
+                            socios socio = objBD.socios.Find(dni);
+                            objBD.socios.Remove(socio);
+
+                            //Se guardan los cambios
+                            objBD.SaveChanges();
+                            MessageBox.Show("Socio eliminado correctamente");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se ha encontrado el socio.");
+                    }
+
+                }
+            }
         }
     }
 }
